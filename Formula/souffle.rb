58edd246cecb361e6c976081844cbd8dc68aeb43
Travis <travis@Traviss-Mac-463.local>
2019-03-05 05:36:06 +0000

    Update to *

--- Formula/souffle.rb
+++ Formula/souffle.rb
@@ -1,8 +1,8 @@
 class Souffle < Formula
   desc "Translator of declarative Datalog programs into the C++ language."
   homepage "https://github.com/souffle-lang/souffle/wiki"
-  url "https://github.com/souffle-lang/souffle/archive/1.2.0.tar.gz"
-  sha256 "c40bda0cf65477cb8c6227018effaa3c32d7394f9d436f5f769719202b5d59c7"
+  url "https://dl.bintray.com/souffle-lang/osx/*.pkg"
+  sha256 ""
 
   depends_on "libtool" => :build
   depends_on "bison" => :build
