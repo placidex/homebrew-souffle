22c59a24e84c0361d142c17bb6c3f90e23b5d8e8
mmcgr <mmcgr@users.noreply.github.com>
2019-03-08 21:42:29 +1100

    Add --head option and update depends

--- Formula/souffle.rb
+++ Formula/souffle.rb
@@ -1,16 +1,26 @@
 class Souffle < Formula
   desc "Translator of declarative Datalog programs into the C++ language."
   homepage "https://github.com/souffle-lang/souffle/wiki"
-  url "https://dl.bintray.com/souffle-lang/osx/souffle-1.5.1-294-g3955d2c7.tar.gz"
-  sha256 "578bc8a15b37cc8d6536007626518a84be9809b1affca5207a641cd5d9313fdd"
+  stable do
+    url "https://dl.bintray.com/souffle-lang/osx/souffle-1.5.1.tar.gz"
+    sha256 "c21357c561297100c1d402f65d3ab1bd01bc743106ca998cef51a3f337f41a7f"
+  end
+  head "https://github.com/souffle-lang/souffle.git", :shallow => false
 
   depends_on "libtool" => :build
   depends_on "bison" => :build
-  depends_on "md5sha1sum" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "libffi" => :build
+  depends_on "mcpp"
+  depends_on "pkg-config" => :build
 
   def install
+    if build.head?
+      system "git", "fetch", "--tags"
+      system "./bootstrap"
+    end
+
     system "./configure", "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
