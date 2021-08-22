425613e2add07307664f8ec780b74e8474e79eac
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2021-08-22 22:07:16 +1000

    Update souffle.rb

--- Formula/souffle.rb
+++ Formula/souffle.rb
@@ -19,7 +19,7 @@ class Souffle < Formula
     end
 
     system "cmake", "-B build -S . -DCMAKE_INSTALL_PREFIX=#{prefix}"
-    system "cmake", "--build build --target install"
+    system "cmake", "--target install"
   end
 
   test do
