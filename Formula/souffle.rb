cb75e6b0a844c3ea5cd05c491fe9e0fd36cca572
Krishna Padmasola <kpadmasola@gmail.com>
2022-11-05 16:50:11 +0530

    Updated formula for Souffle 2.3 and MacOS 13.0

--- Formula/souffle.rb
+++ Formula/souffle.rb
@@ -1,10 +1,11 @@
 class Souffle < Formula
   desc "Logic Programming Language based on Datalog."
   homepage "https://souffle-lang.github.io"
+
   stable do
-    url "https://github.com/souffle-lang/souffle/archive/refs/tags/2.1.tar.gz"
-    sha256 "866b5aeaf88c0c5c2c1b6cb2b33faa6a0084154f5396e644f11767d6fe82b1d6"
+    url "https://github.com/souffle-lang/souffle", :using => :git, :tag => "2.3", :shallow => false
   end
+
   head "https://github.com/souffle-lang/souffle.git", :shallow => false
 
   depends_on "bison" => :build
@@ -19,11 +20,20 @@ class Souffle < Formula
       system "git", "fetch", "--tags"
     end
 
-    system "cmake", "-B", "build", "-S", ".", "-DCMAKE_INSTALL_PREFIX=#{prefix}","-DSOUFFLE_GIT=OFF", "-DSOUFFLE_BASH_COMPLETION=OFF", "-DCMAKE_C_COMPILER=gcc-11", "-DCMAKE_CXX_COMPILER=g++-11"
-    system "cmake", "--build", "build", "--target", "install", "-j17"
+    system "cmake", "-B", "build", "-S", ".", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DSOUFFLE_BASH_COMPLETION=OFF", "-DCMAKE_CXX_COMPILER=g++-11"
+    system "cmake", "--build", "build", "--target", "install", "-j"
   end
 
   test do
-    system "false"
+    assert_equal <<-EOS, shell_output("#{bin}/souffle --version 2>&1")
+----------------------------------------------------------------------------
+Version: 2.3
+----------------------------------------------------------------------------
+Copyright (c) 2016-22 The Souffle Developers.
+Copyright (c) 2013-16 Oracle and/or its affiliates.
+All rights reserved.
+============================================================================
+
+EOS
   end
 end
